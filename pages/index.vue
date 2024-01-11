2982f59a906267643245ecc1ffd3bfcf247c9a5d
Anastasiia Shevtsova <anastasiia08@ukr.net>
2024-01-11 12:37:56 +0100

    Add basic meta tags on the homepage. Update favicon.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>
+  <div
+    class="m-4 flex h-10 cursor-pointer items-center justify-center rounded-[7px] bg-red-500 shadow-xl"
+  >
     Hello world!
   </div>
 </template>
 
 <script setup lang="ts">
+useHead({
+  title: 'Mirabellen',
+  meta: [
+    { name: 'description', content: 'Cosmetics shop' },
+    { name: 'og:title', content: 'Mirabellen - cosmetics' },
+  ],
+})
 </script>
